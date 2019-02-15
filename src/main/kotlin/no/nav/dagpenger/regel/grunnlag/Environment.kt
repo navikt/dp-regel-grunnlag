204eaac67a5811184d1356b7ba9d2223db5d331e
Atle Nordland <atle.nordland@nav.no>
2019-02-15 12:09:47 +0100

    Change port

--- src/main/kotlin/no/nav/dagpenger/regel/grunnlag/Environment.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/grunnlag/Environment.kt
@@ -4,7 +4,7 @@ data class Environment(
     val username: String = getEnvVar("SRVDP_REGEL_GRUNNLAG_USERNAME"),
     val password: String = getEnvVar("SRVDP_REGEL_GRUNNLAG_PASSWORD"),
     val bootstrapServersUrl: String = getEnvVar("KAFKA_BOOTSTRAP_SERVERS", "localhost:9092"),
-    val httpPort: Int? = 8095
+    val httpPort: Int? = 8098
 )
 
 fun getEnvVar(varName: String, defaultValue: String? = null) =
