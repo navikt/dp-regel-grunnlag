1a4aee802f1f419588252042a8d4753402d99a88
Geir André Lund <geir.andre.lund@nav.no>
2019-03-26 14:29:28 +0100

    Removed unused code..

--- src/main/kotlin/no/nav/dagpenger/regel/grunnlag/InntektsOppjusteringsBeregning.kt
+++
@@ -1,8 +0,0 @@
-package no.nav.dagpenger.regel.grunnlag
-
-import java.math.BigDecimal
-
-class InntektsOppjusteringsBeregning(inntekt: BigDecimal, gjeldendeG: BigDecimal, månedsG: BigDecimal) {
-
-    val resultat: BigDecimal = inntekt * (gjeldendeG / månedsG)
-}
