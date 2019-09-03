50256e8f136ca1d3384b00398d9a20be7081fa63
André Roaldseth <andre.roaldseth@nav.no>
2019-09-03 09:46:59 +0200

    Enable gjustering test in preprod

--- src/main/kotlin/no/nav/dagpenger/regel/grunnlag/Configuration.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/grunnlag/Configuration.kt
@@ -21,7 +21,8 @@ private val devProperties = ConfigurationMap(
     mapOf(
         "kafka.bootstrap.servers" to "b27apvl00045.preprod.local:8443,b27apvl00046.preprod.local:8443,b27apvl00047.preprod.local:8443",
         "application.profile" to Profile.DEV.toString(),
-        "application.httpPort" to "8098"
+        "application.httpPort" to "8098",
+        "feature.gjustering" to true.toString()
     )
 )
 private val prodProperties = ConfigurationMap(
