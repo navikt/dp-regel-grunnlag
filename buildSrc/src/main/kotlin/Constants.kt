f2b11bc6ab3a0b4bbd6d3424856b42b324014cca
Geir André Lund <geir.andre.lund@nav.no>
2019-09-17 07:14:13 +0200

    Updated dagpenger-streams which: * introduces health check - default health check checks state of kafka stream * add an option to disable health check api altogether

--- buildSrc/src/main/kotlin/Constants.kt
+++ buildSrc/src/main/kotlin/Constants.kt
@@ -34,7 +34,7 @@ object Dagpenger {
         const val ktorUtils = "com.github.navikt.dp-biblioteker:ktor-utils:$version"
     }
 
-    const val Streams = "com.github.navikt:dagpenger-streams:2019.08.06-10.54.063ebf321402"
+    const val Streams = "com.github.navikt:dagpenger-streams:2019.08.13-10.25.4b7bef10fc88"
     const val Events = "com.github.navikt:dagpenger-events:2019.08.06-10.38.92d9930cd257"
 }
 
@@ -211,6 +211,3 @@ object Wiremock {
     const val version = "2.21.0"
     const val standalone = "com.github.tomakehurst:wiremock-standalone:$version"
 }
-
-
-
