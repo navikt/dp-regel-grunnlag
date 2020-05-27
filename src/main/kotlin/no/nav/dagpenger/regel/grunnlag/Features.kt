10f00c309efa26e0a1d21dc7409464a72e0dc2e2
Giao The Cung <giaothe@gmail.com>
2020-05-27 10:37:49 +0200

    TEMP: autogenerate toString

--- src/main/kotlin/no/nav/dagpenger/regel/grunnlag/Features.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/grunnlag/Features.kt
@@ -1,6 +1,6 @@
 package no.nav.dagpenger.regel.grunnlag
 
-class Features(private val features: Map<String, Boolean>) {
+data class Features(private val features: Map<String, Boolean>) {
     fun isEnabled(feature: String): Boolean {
         return features.getOrDefault(feature, false)
     }
