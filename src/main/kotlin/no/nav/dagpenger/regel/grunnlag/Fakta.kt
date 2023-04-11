6c9b53aa38a75b3cf07f0cf4a5b404ce223000c2
Geir André Lund <geir.andre.lund@nav.no>
2023-04-11 16:17:07 +0200

    Nye verdi for g-justeringstest DAG-779

--- src/main/kotlin/no/nav/dagpenger/regel/grunnlag/Fakta.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/grunnlag/Fakta.kt
@@ -70,7 +70,7 @@ data class Fakta(
 }
 
 internal fun isThisGjusteringTest(regelverksdato: LocalDate): Boolean {
-    val gVirkning = LocalDate.of(2022, 4, 24)
+    val gVirkning = LocalDate.of(2023, 3, 27)
     val isRegelverksdatoAfterGjustering = regelverksdato.isAfter(gVirkning.minusDays(1))
     return Grunnlag.unleash.isEnabled(GJUSTERING_TEST) && isRegelverksdatoAfterGjustering
 }
