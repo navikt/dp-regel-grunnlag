30948d54759c0565079fd5a87201866008145a7d
celinetendam <celine.ten.dam@nav.no>
2023-04-26 09:43:08 +0200

    Refaktorering
    Co-authored-by: Giao The Cung<giaothe@gmail.com>

--- src/main/kotlin/no/nav/dagpenger/regel/grunnlag/beregning/TrettiseksMånedersBeregning.kt
+++ src/main/kotlin/no/nav/dagpenger/regel/grunnlag/beregning/TrettiseksMånedersBeregning.kt
@@ -9,57 +9,53 @@ import java.util.EnumSet
 
 abstract class TrettiseksMånedersBeregning(
     val inntektKlasser: EnumSet<InntektKlasse>,
-    beregningsregel: String
+    beregningsregel: String,
 ) :
     GrunnlagBeregning(beregningsregel) {
     private val antallDesimaler = 20
     private val roundingMode = RoundingMode.HALF_UP
-
-    private fun avkortetFør17_12_21(fakta: Fakta): Resultat {
-        val uavkortet = uavkortet(fakta)
-
-        val seksGangerGrunnbeløp = fakta.grunnbeløpVedBeregningsdato().verdi.multiply(BigDecimal(6))
-        val avkortet = if (uavkortet > seksGangerGrunnbeløp) seksGangerGrunnbeløp else uavkortet
-
-        return BeregningsResultat(uavkortet, avkortet, beregningsregel)
-    }
-
-    private fun avkortetEtter17_12_21(fakta: Fakta): Resultat {
-
-        val uavkortetFørstePeriode = fakta.oppjusterteInntekterFørstePeriode(inntektKlasser)
-        val uavkortetAndrePeriode = fakta.oppjusterteInntekterAndrePeriode(inntektKlasser)
-        val uavkortetTredjePeriode = fakta.oppjusterteInntekterTredjePeriode(inntektKlasser)
-        val uavkortet = uavkortet(fakta)
-
-        val seksGangerGrunnbeløp = fakta.grunnbeløpVedBeregningsdato().verdi.multiply(BigDecimal(6))
-
-        val avkortetFørste = if (uavkortetFørstePeriode > seksGangerGrunnbeløp) seksGangerGrunnbeløp else uavkortetFørstePeriode
-
-        val avkortetAndre = if (uavkortetAndrePeriode > seksGangerGrunnbeløp) seksGangerGrunnbeløp else uavkortetAndrePeriode
-
-        val avkortetTredje = if (uavkortetTredjePeriode > seksGangerGrunnbeløp) seksGangerGrunnbeløp else uavkortetTredjePeriode
-
-        val avkortet = (avkortetFørste + avkortetAndre + avkortetTredje).divide(3.toBigDecimal(), antallDesimaler, RoundingMode.HALF_UP)
-
-        return BeregningsResultat(uavkortet, avkortet, beregningsregel)
-    }
-
-    private fun uavkortet(fakta: Fakta): BigDecimal {
-        val uavkortetFørstePeriode = fakta.oppjusterteInntekterFørstePeriode(inntektKlasser)
-        val uavkortetAndrePeriode = fakta.oppjusterteInntekterAndrePeriode(inntektKlasser)
-        val uavkortetTredjePeriode = fakta.oppjusterteInntekterTredjePeriode(inntektKlasser)
-
-        return (uavkortetFørstePeriode + uavkortetAndrePeriode + uavkortetTredjePeriode).divide(3.toBigDecimal(), antallDesimaler, roundingMode)
-    }
+    private val grensedato = LocalDate.of(2021, 12, 17) // ifbm feilsaker om avkorting mot 6G må vi ha to praksiser for beregning for å rydde opp
+    private lateinit var seksGangerGrunnbeløp: BigDecimal
 
     override fun isActive(fakta: Fakta): Boolean = !(fakta.lærling && fakta.beregningsdato.erKoronaPeriode())
 
     override fun calculate(fakta: Fakta): Resultat {
+        seksGangerGrunnbeløp = fakta.grunnbeløpVedBeregningsdato().verdi.multiply(BigDecimal(6))
 
-        return if (fakta.beregningsdato.isBefore(LocalDate.of(2021, 12, 17))) {
-            avkortetFør17_12_21(fakta)
+        val inntekter = Triple(
+            fakta.oppjusterteInntekterFørstePeriode(inntektKlasser),
+            fakta.oppjusterteInntekterAndrePeriode(inntektKlasser),
+            fakta.oppjusterteInntekterTredjePeriode(inntektKlasser)
+        )
+
+        val uavkortet = inntekter.toList().sumOf { it }.divide(
+            3.toBigDecimal(),
+            antallDesimaler,
+            roundingMode
+        )
+
+        val avkortet = if (fakta.beregningsdato.isBefore(grensedato)) {
+            avkortetFør17_12_21(uavkortet)
         } else {
-            avkortetEtter17_12_21(fakta)
+            avkortetEtter17_12_21(inntekter)
         }
+
+        return BeregningsResultat(uavkortet, avkortet, beregningsregel)
+    }
+
+    private fun avkortetFør17_12_21(uavkortet: BigDecimal) =
+        if (uavkortet > seksGangerGrunnbeløp) seksGangerGrunnbeløp else uavkortet
+
+    private fun avkortetEtter17_12_21(
+        inntekter: Triple<BigDecimal, BigDecimal, BigDecimal>,
+    ): BigDecimal {
+
+        return inntekter.toList().sumOf { inntekt ->
+            if (inntekt > seksGangerGrunnbeløp) seksGangerGrunnbeløp else inntekt
+        }.divide(
+            3.toBigDecimal(),
+            antallDesimaler,
+            RoundingMode.HALF_UP
+        )
     }
 }
