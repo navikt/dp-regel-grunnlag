11ede59274fa53925a1e799f1c051d3d76be5eb2
Marius Eriksen <marius.eriksen@nav.no>
2023-12-06 08:51:56 +0100

    Bruk nyeste versjon av dagpenger-events, slik at testene kjører

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ dependencies {
     implementation(kotlin("stdlib"))
     implementation(libs.rapids.and.rivers)
 
-    implementation("com.github.navikt:dagpenger-events:2023081713361692272216.01ab7c590338")
+    implementation("com.github.navikt:dagpenger-events:20231204.ee1cc3")
     implementation("com.github.navikt:dp-grunnbelop:2023.05.24-15.26.f42064d9fdc8")
 
     implementation("de.huxhorn.sulky:de.huxhorn.sulky.ulid:8.3.0")
